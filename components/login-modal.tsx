2175d980baea9f4c92af6779818e57abddf39a93
Liden <lninyow@gmail.com>
2024-04-12 12:29:31 +0800

    smol fix

--- components/login-modal.tsx
+++ components/login-modal.tsx
@@ -29,6 +29,9 @@ export default function LoginModal({ id }: LoginModalProps) {
                 placeholder="Input your password"
                 className="input input-bordered w-full "
               />
+              <a className="link-hover link mt-2 self-end text-sm">
+                Dont have an account? Register here
+              </a>
             </label>
 
             <div className="modal-action">
