4949dcd30e240a85f92085df5305aff5890b1130
Nathaniel Tampus <nate@mugnavo.com>
2024-04-15 20:48:57 +0800

    dashboard placeholder

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      Dashboard layout, put sidebar/navbar here
+      {children}
+    </div>
+  );
+}
