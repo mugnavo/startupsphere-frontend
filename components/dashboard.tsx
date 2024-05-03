51ef05c07e4fda012f9f6ff5c8042d91a30107fd
Nathaniel Tampus <nate@mugnavo.com>
2024-05-03 16:13:32 +0800

    Revert "remove body from delete fetch"
    This reverts commit 53f742b9aacbd5bf786dfae0feb6c3ecdfc87e81.

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -104,6 +104,7 @@ export default function DashboardComponent({
                     onClick={async () => {
                       await fetch(`/api/startups?id=${startup.id}`, {
                         method: "DELETE",
+                        body: JSON.stringify({ id: startup.id }),
                       });
                       router.refresh();
                     }}
