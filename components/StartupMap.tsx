ae82798016837c057e5bd978b76011db16807030
Nathaniel Tampus <nate@mugnavo.com>
2024-09-11 22:28:20 +0800

    3d map improvements

--- components/StartupMap.tsx
+++ components/StartupMap.tsx
@@ -5,42 +5,14 @@ import { ChevronLeft } from "lucide-react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
-import type { FillExtrusionLayer, MapLayerMouseEvent } from "react-map-gl";
-import Map, { Layer, Marker, useMap } from "react-map-gl";
+import type { MapLayerMouseEvent } from "react-map-gl";
+import Map, { Marker, useMap } from "react-map-gl";
 
 import { useInteractiveMap } from "~/context/hooks";
 import { startupControllerGetAll } from "~/lib/api";
 import { Startup } from "~/lib/schemas";
 import CustomPin from "./map/CustomPin";
 import Geocoder from "./map/Geocoder";
-
-const building3dLayer: FillExtrusionLayer = {
-  // from https://docs.mapbox.com/mapbox-gl-js/example/3d-buildings/
-  id: "add-3d-buildings",
-  source: "composite",
-  "source-layer": "building",
-  filter: ["==", "extrude", "true"],
-  type: "fill-extrusion",
-  minzoom: 15,
-  paint: {
-    "fill-extrusion-color": "#aaa",
-
-    // Use an 'interpolate' expression to
-    // add a smooth transition effect to
-    // the buildings as the user zooms in.
-    "fill-extrusion-height": ["interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "height"]],
-    "fill-extrusion-base": [
-      "interpolate",
-      ["linear"],
-      ["zoom"],
-      15,
-      0,
-      15.05,
-      ["get", "min_height"],
-    ],
-    "fill-extrusion-opacity": 0.6,
-  },
-};
 
 const geocode = new GeocodingCore({ accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN! });
 
@@ -108,6 +80,21 @@ export default function StartupMap() {
     }
   }, [dashboardSelection.previewLocation, mainMap]);
 
+  useEffect(() => {
+    if (!mainMap) return;
+
+    mainMap.on("style.load", () => {
+      // hide labels
+      mainMap.setConfigProperty("basemap", "showPlaceLabels", false);
+      mainMap.setConfigProperty("basemap", "showTransitLabels", false);
+      mainMap.setConfigProperty("basemap", "showPointOfInterestLabels", false);
+
+      // theme
+      mainMap.setConfigProperty("basemap", "theme", "default");
+      mainMap.setConfigProperty("basemap", "lightPreset", "day");
+    });
+  }, [mainMap]);
+
   return (
     <div className="relative h-screen overflow-hidden">
       {dashboardSelection.active && (
@@ -163,7 +150,7 @@ export default function StartupMap() {
         onMove={(evt) => setViewState(evt.viewState)}
         style={{ width: "100%", height: "100%" }}
         onClick={onClick}
-        mapStyle="mapbox://styles/mapbox/streets-v12"
+        mapStyle="mapbox://styles/mapbox/standard"
         projection={{ name: "globe" }}
       >
         {dashboardSelection.active && (
@@ -207,8 +194,8 @@ export default function StartupMap() {
               <div className="absolute -bottom-6 text-center text-sm font-semibold">
                 {startup.name}
               </div>
-              <div className="absolute bottom-8 z-50 hidden h-auto w-56 flex-col rounded-md bg-slate-50  shadow group-hover:z-50 group-hover:flex">
-                <div className=" flex h-32 w-full flex-col bg-yellow-400">
+              <div className="absolute bottom-8 z-50 hidden h-auto w-56 flex-col rounded-md bg-slate-50 shadow group-hover:z-50 group-hover:flex">
+                <div className="flex h-32 w-full flex-col bg-yellow-400">
                   {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img
                     className="h-[122px] rounded-md object-cover"
@@ -228,7 +215,6 @@ export default function StartupMap() {
             </div>
           </Marker>
         ))}
-        <Layer {...building3dLayer} />
       </Map>
     </div>
   );
