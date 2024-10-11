78408381a2fc7af6b0c4171eb68bcdccc413a4c1
Nathaniel Tampus <nate@mugnavo.com>
2024-10-11 17:04:53 +0800

    update auth context provider

--- context/Provider.tsx
+++ context/Provider.tsx
@@ -2,7 +2,7 @@
 import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 import { MapProvider } from "react-map-gl";
-import { authControllerMe } from "~/lib/api";
+import { usersControllerGetProfile } from "~/lib/api";
 import { User } from "~/lib/schemas";
 import { DashboardSelection, LocationData } from "~/lib/types";
 import { withAuth } from "~/lib/utils";
@@ -30,7 +30,7 @@ export function ContextProvider({ children }: { children: React.ReactNode }) {
       setUser(user);
 
       if (user?.id) {
-        const updatedUser = await authControllerMe(user.id, withAuth);
+        const updatedUser = await usersControllerGetProfile(withAuth);
         if (updatedUser && updatedUser.data) setUser(updatedUser.data);
       }
     }
