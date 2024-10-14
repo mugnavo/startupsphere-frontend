a78fff770bd387817f2cdcf9e4b25e6444d5a98f
Nathaniel Tampus <nate@mugnavo.com>
2024-10-14 13:20:48 +0800

    simpler auth flow

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,22 +3,17 @@
 import { jwtDecode } from "jwt-decode";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useEffect } from "react";
-import { useSession } from "~/context/hooks";
 import { User } from "~/lib/schemas";
 
 function AutoAuthFlow() {
   const searchParams = useSearchParams();
-  const { user, setUser } = useSession();
 
   const token = searchParams.get("token");
 
   useEffect(() => {
     if (!token) return;
     try {
-      const user = jwtDecode(token) as User;
-      if (user?.email) {
-        setUser(user);
-      }
+      jwtDecode(token) as User;
       localStorage.setItem("jwt", token);
     } catch {
       console.error("Invalid token");
@@ -27,7 +22,7 @@ function AutoAuthFlow() {
       window.open("about:blank", "_self");
       window.close();
     }
-  }, [token, setUser]);
+  }, [token]);
 
   return <div>page</div>;
 }
